b2ed2c65d7bc7ed7f12060dee2d9e2f6602e57f4
nareshungarala396 <nareshungarala220@gmail.com>
2025-11-10 12:51:08 +0530

    roboshop

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    type = string
+}
+variable "environment" {
+    type = string
+}
+
+variable "sg_description" {
+    type = string
+    default = {}
+}
+variable "vpc_id" {
+    type = string
+}
+
+variable "sg_tags" {
+    type = map
+    default = {}
+}
+
+variable "sg_name" {
+    type = string
+}
